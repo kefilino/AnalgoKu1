b962ef1660770f1bb4226a978c2f00929118436d
Kefilino Khalifa <kefilino@live.com>
2020-03-02 22:00:47 +0700

    lieur bro

--- worksheet01.cpp
+++ worksheet01.cpp
@@ -10,9 +10,9 @@
 #include <iostream>
 using namespace std;
 
-bool isFree(char match[5]) {
+bool isFree(int match[5]) {
     for (int i = 0; i < 5; i++) {
-        if (match[i] == '\0')
+        if (match[i] == -1)
             return true;
     }
     return false;
@@ -26,15 +26,40 @@ bool hasProposed(int proposal[5]) {
     return true;
 }
 
-void fungsiGS(int pref[][5], char match[5]) {
+bool prefers(int wpref[][5], int mpref[][5], int w, int m1, int m2) {
+    int m1idx = -1, m2idx = -1;
+    for (int i = 0; i < 5; i++) {
+        if (wpref[w][i] == m1)
+            m1idx = i;
+        else if (wpref[w][i] == m2)
+            m2idx = i;
+    }
+    if (m1idx < m2idx)
+        return true;
+    else
+        return false;
+}
+
+void fungsiGS(int mpref[][5], int wpref[][5], int match[5]) {
     int proposal[5] = {0}; // Jumlah wanita yang telah dilamar
     int taken[5] = {-1}; // Wanita ke-i taken oleh siapa
 
     while (isFree(match) && !hasProposed(proposal)) {
         for (int i = 0; i < 5; i++) {
-            if (match[i] == '\0') {
-
+            if (match[i] == -1) {
+                if (proposal[i] == 5)
+                    proposal[i] = 0;
+                int temp = mpref[i][proposal[i]]; // Preferensi wanita dari pria saat ini
+                if (taken[temp] == -1) {
+                    match[i] = temp;
+                    taken[temp] = i;
+                } else if (prefers(wpref, mpref, temp, i, taken[temp])) {
+                    match[taken[temp]] = -1;
+                    match[i] = temp;
+                    taken[temp] = i;
+                }
             }
+            proposal[i]++;
         }
     }
 }
@@ -42,13 +67,22 @@ void fungsiGS(int pref[][5], char match[5]) {
 int main()
 {
     //Profil preferensi pria
-    const int mpp[5][5] = {
+    int mpp[5][5] = {
         {1, 0, 3, 4, 2},
         {3, 1, 0, 2, 4},
         {1, 4, 2, 3, 0},
         {0, 3, 2, 1, 4},
         {1, 3, 0, 4, 2}
     };
-    char match[5] = {'\0'}; //Pasangan dari pria
-    
+    //Profil preferensi wanita
+    int wpp[5][5] = {
+        {4, 0, 1, 3, 2},
+        {2, 1, 3, 0, 4},
+        {1, 2, 3, 4, 0},
+        {0, 4, 3, 2, 1},
+        {3, 1, 4, 2, 0}
+    };
+    int match[5] = {-1, -1, -1, -1, -1}; //Pasangan dari pria
+    fungsiGS(mpp, wpp, match);
+    cout << match[0] << endl << match[1] << endl << match[2] << endl << match[3] << endl << match[4];
 }
